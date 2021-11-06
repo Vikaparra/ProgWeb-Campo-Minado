2ee4720ff1a5bbfaf3a09e34a9e220034a268994
Vitória Parra <vikaparra@gmail.com>
2021-11-06 01:40:00 -0300

    inserindo botões sair
    mudando as funções de get element by id para get by class

--- View/Shared/assets/telaJogo.js
+++ View/Shared/assets/telaJogo.js
@@ -5,16 +5,29 @@ var tempo = document.getElementById("tempo");
 
 startTimer(tempo);
 
-var bombas = document.getElementById("numero-bombas");
-bombas.innerText = ("Numero de Bombas: " + tabuleiro.numeroBombas);
+var bombas = document.getElementsByClassName("numero-bombas");
+for (var i = 0; i < bombas.length; i++) {
+    bombas[i].innerText = ("Numero de Bombas: " + tabuleiro.numeroBombas);
+}
 
-var grid = document.getElementById("grid");
-grid.innerText = ("Grid: " + tabuleiro.numeroCelulas + "x" + tabuleiro.numeroCelulas);
+var grid = document.getElementsByClassName("grid");
+for (var i = 0; i < grid.length; i++) {
+    grid[i].innerText = ("Grid: " + tabuleiro.numeroCelulas + "x" + tabuleiro.numeroCelulas);
+}
 
-var alterarConfigs = document.getElementById("botao-config");
-alterarConfigs.addEventListener("click", ()=>{
-    document.location.href = ("../configuracoes/configs.html");
-});
+var alterarConfigs = document.getElementsByClassName('botao-config');
+for (var i = 0; i < alterarConfigs.length; i++) {
+    alterarConfigs[i].addEventListener('click',  ()=>{
+        document.location.href = ("../configuracoes/configs.html");
+    }, false);
+}
+
+var sair = document.getElementsByClassName('botao-sair-jogo');
+for (var i = 0; i < sair.length; i++) {
+    sair[i].addEventListener('click',  ()=>{
+        document.location.href = ("../tela_menu_principal/tela_menu_principal.html");
+    }, false);
+}
 
 var trapacaBtn = document.getElementById("trapaca");
 trapacaBtn.addEventListener("click", ()=>{
