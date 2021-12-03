e0b165d5326c5012899379567a8117d1c083bdb5
Vitória Parra <vikaparra@gmail.com>
2021-12-03 00:19:33 -0300

    tentando fazer cadastro funcionar

--- Controller/cadastro.php
+++ Controller/cadastro.php
@@ -9,34 +9,15 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $data = json_decode(file_get_contents('php://input'), true);
+
+        $result = $conn->exec("
+            INSERT INTO user VALUES (".$data['id_user'].",".$data['nome'].",".$data['data_nascimento'].",".$data['cpf'].",".$data['telefone'].",".$data['email'].",".$data['username'].",".$data['senha'].")");
         
-        // echo "<script language=’javascript’ type=’text/javascript’>system.log('ENTROU');</script>";
-
-
-        $statement = $conn->query("SELECT * FROM user WHERE id_user = 1");
-        $row = $statement->fetch(PDO::FETCH_ASSOC);
-        if ($row == ""){
-            echo json_encode(["result" => 201]);
+        if ($result->rowCount() > 0) {
+           echo json_encode(["result" => 201]);// realizou as mudanças
+        }else{
+            echo json_encode(["result" => 200]); //está ok mas não fez musdanças
         }
-        echo json_encode($row); 
-
-
-        // $result = $conn->exec("
-        //     INSERT INTO user 
-        //     SET 
-        //     nome = '".$data["nome"]."',
-        //     data_nascimento = '".$data["data_nascimento"]."',
-        //     telefone = '".$data["telefone"]."',
-        //     email = '".$data["email"]."',
-        //     senha = '".$data["senha"]."'
-        //     WHERE id_user = ".$data["id_user"]."
-        // ");
-        
-        // if ($result > 0) {
-           // echo json_encode(["result" => 201]);// realizou as mudanças
-        // }else{
-        //     echo json_encode(["result" => 200]); //está ok mas não fez musdanças
-        // }
 
                     
 
