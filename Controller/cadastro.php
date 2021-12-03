2dfe2bd73bdb584845039651528c4650ebd8dc6b
Vitória Parra <vikaparra@gmail.com>
2021-12-03 12:56:23 -0300

    erros no cadastro corrigidos
    verificando o post com isset() e utilizando prepare e execute

--- Controller/cadastro.php
+++ Controller/cadastro.php
@@ -56,17 +56,17 @@
         // $result = $conn->query($sql)->rowCount();
 
 
-
-        $sql = "INSERT INTO user (id_user, username, nome_user, cpf, email, data_nascimento, telefone, senha) VALUES (?,?,?,?,?,?,?,?)";
-        $result= $conn->prepare($sql);
-        $resultado = $result->execute([$data["id_user"], $data["username"], $data["nome"], $data["cpf"], $data["email"], $data["data_nascimento"], $data["telefone"], $data["senha"]]);
-        if ($resultado > 0) {
-           echo json_encode(["result" => 201]);// realizou as mudanças
-        }else{
-            echo json_encode(["result" => 200]); //está ok mas não fez musdanças
+        if (isset($data["username"])){
+            $sql = "INSERT INTO user (username, nome_user, cpf, email, data_nascimento, telefone, senha) VALUES (?,?,?,?,?,?,?)";
+            $result= $conn->prepare($sql);
+            $resultado = $result->execute([$data["username"], $data["nome"], $data["cpf"], $data["email"], $data["data_nascimento"], $data["telefone"], $data["senha"]]);
+            if ($resultado > 0) {
+               echo json_encode(["result" => 201]);// realizou as mudanças
+            }else{
+                echo json_encode(["result" => 200]); //está ok mas não fez musdanças
+            }
         }
-
-                    
+                
 
     } catch (PDOException $err) {
         echo json_encode(["error" => "conexão falhou: ". $err->getMessage()]);
